86145c52c32b2e7efcdf2229068956844462aa01
TResulHK78 <76772167+TResulHK78@users.noreply.github.com>
2026-03-04 20:23:42 +0300

    sinyal.py

--- sinyal.py
+++ sinyal.py
@@ -6,7 +6,7 @@ import time
 import traceback
 
 # --- AYARLAR ---
-TELEGRAM_TOKEN = "7968551890:AAFmtuxAvIEhpYVg7m8NL2TjROLQPgJxvzA"
+TELEGRAM_TOKEN = "7968551890:AAFet_Y-e_1LvqKqcy0PmQn7wEC1o3KJdmg"
 CHAT_ID = "@rhksinyal"
 TIMEFRAME = "15m"
 LIMIT = 1001
