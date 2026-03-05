c6108d1c5c38133446ddb3aace5eed90601ef795
TResulHK78 <76772167+TResulHK78@users.noreply.github.com>
2026-03-05 16:12:02 +0300

    sinyal.py

--- sinyal.py
+++ sinyal.py
@@ -9,15 +9,18 @@ import gc
 
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "🚀 Keskin Nişancı Botu 7/24 Aktif ve Çalışıyor!"
+    return "🚀 Bot Canavar Gibi Ayakta!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render'ın otomatik atadığı kapıyı (PORT) bulur, uyumsuzluğu bitirir!
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
